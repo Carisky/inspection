83e835a7eac9c0c66b23ae767d6278f6f08ad378
Yehor <egorkorsungit@gmail.com>
2025-02-11 21:58:35 +0100

    Header+Footer registration as components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Builder } from '@builder.io/react';
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+Builder.registerComponent(Header, {
+  name: 'Header',
+});
+
+Builder.registerComponent(Footer, {
+  name: 'Footer',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
