13fd76c18cfca1a20bbfdfd4432a1100bb3186c1
Yehor <egorkorsungit@gmail.com>
2025-02-11 21:57:53 +0100

    Added demo footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+// components/Footer.tsx
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Footer text here &copy; 2025</p>
+    </footer>
+  );
+};
+
+export default Footer;
