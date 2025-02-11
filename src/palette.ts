532a2ba384b479cc95621b865cca1bc2ad85e24b
Yehor <egorkorsungit@gmail.com>
2025-02-11 21:57:25 +0100

    added pallete with few collors

---
+++ src/palette.ts
@@ -0,0 +1,12 @@
+const pallete = {
+  elements: {
+    nav: "#5c4c6e",
+    nav_element_hover: "#292332",
+  },
+  common_colors:{
+    white:"#fff",
+    main_color:"#8d004c",
+    background:"#dedede"
+  }
+};
+export default pallete;
