862a8966785fbef6d258e49e775f977bd8454166
Yehor <egorkorsungit@gmail.com>
2025-02-18 14:38:05 +0100

    wrapped up the app with LocaleProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Register from "@/Register";
+import { LocaleProvider } from "@/context/LocaleContext"; // Предполагается, что LocaleProvider в этом пути
 
 Register.init();
 
-
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <LocaleProvider>
+      <Component {...pageProps} />
+    </LocaleProvider>
+  );
 }
