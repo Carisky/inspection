7eb3862a3c1e365ca020ec308c6c380f5b3c455a
Yehor <egorkorsungit@gmail.com>
2025-02-18 12:42:27 +0100

    Registerd ArticlePreview component for Builder ui page editor

--- src/Register.ts
+++ src/Register.ts
@@ -3,6 +3,7 @@ import Header from "./components/BuilderIO/Header";
 import Footer from "./components/BuilderIO/Footer";
 import ImagesCarousel from "./components/BuilderIO/ImagesCarousel";
 import FadeSlider from "./components/BuilderIO/FadeSlider"; // Импортируем FadeSlider
+import ArticlePreview from "./components/BuilderIO/ArticlePreview";
 
 class Register {
   static init = () => {
@@ -67,6 +68,34 @@ class Register {
           type: "color",
           defaultValue: "#ffffff",
           helperText: "Цвет текста",
+        },
+      ],
+    });
+
+    // Регистрация ArticlePreview
+    Builder.registerComponent(ArticlePreview, {
+      name: "ArticlePreview",
+      inputs: [
+        {
+          name: "title",
+          type: "string",
+          defaultValue: "Sample Title",
+        },
+        {
+          name: "image",
+          type: "file",
+          allowedFileTypes: ["jpeg", "jpg", "png", "webp"],
+          defaultValue: "https://via.placeholder.com/300",
+        },
+        {
+          name: "excerpt",
+          type: "string",
+          defaultValue: "Sample excerpt...",
+        },
+        {
+          name: "slug",
+          type: "string",
+          defaultValue: "sample-article",
         },
       ],
     });
