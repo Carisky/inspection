82a772b9aea20706800c554cf998f8af5de8f738
Yehor <egorkorsungit@gmail.com>
2025-02-19 14:11:03 +0100

    implemented W.K contacts for multiple usage

---
+++ src/components/BuilderIO/Contacts/Marketing/Kwiatkowska.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import pallete from "@/palette";
+
+interface KwiatkowskaProps {
+  contacts?: string[];
+}
+
+const Kwiatkowska: React.FC<KwiatkowskaProps> = ({
+  contacts = [
+    "Wiktoria Kwiatkowska",
+    "Kierownik Działu Marketingu",
+    "kom. +48 608 675 834",
+    "tel. +48 322 822 062",
+    "w.kwiatkowska@tsl-silesia.com.pl",
+  ],
+}) => {
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+      {contacts.map((contact, index) => (
+        <Box key={index} sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+          <CheckCircle sx={{ fontSize: 24, color: pallete.common_colors.main_color }} />
+          <Typography sx={{ fontSize: "18px" }} variant="body1">
+            {contact}
+          </Typography>
+        </Box>
+      ))}
+    </Box>
+  );
+};
+
+export default Kwiatkowska;
