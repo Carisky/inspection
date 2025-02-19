30540f9c20165d2a2cacb1128994466437f6cf4b
Yehor <egorkorsungit@gmail.com>
2025-02-19 14:08:21 +0100

    Deleted provider from app for page loading speed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,9 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Register from "@/Register";
-import { LocaleProvider } from "@/context/LocaleContext"; // Предполагается, что LocaleProvider в этом пути
 
 Register.init();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <LocaleProvider>
-      <Component {...pageProps} />
-    </LocaleProvider>
-  );
+  return <Component {...pageProps} />;
 }
