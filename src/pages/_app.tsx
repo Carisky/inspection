543878e70fcaeda537728039ec25d6d15536c31c
Yehor <egorkorsungit@gmail.com>
2025-02-20 13:50:12 +0100

    added components for data tracking and analitycs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Register from "@/Register";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 Register.init();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+      <Analytics />
+    </>
+  );
 }
