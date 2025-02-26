9bcb1fe977ef55d8cc58bf0bfd94ffd2a5b48918
Yehor <egorkorsungit@gmail.com>
2025-02-26 13:28:47 +0100

    added all social medias links to list

--- src/components/BuilderIO/SocialMediaList.tsx
+++ src/components/BuilderIO/SocialMediaList.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { Box } from "@mui/material";
-import { Telegram, WhatsApp } from "@mui/icons-material";
+import {
+  Instagram,
+  Facebook,
+  Telegram,
+  WhatsApp,
+  LinkedIn,
+  YouTube,
+} from "@mui/icons-material";
 
 interface SocialMediaItem {
   name: string;
@@ -10,13 +17,55 @@ interface SocialMediaItem {
 }
 
 const socialMediaList: SocialMediaItem[] = [
-  { name: "Telegram", url: "https://t.me", color: "#0088cc", icon: <Telegram /> },
-  { name: "WhatsApp", url: "https://wa.me", color: "#25D366", icon: <WhatsApp /> },
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com/tsl_silesia/",
+    color: "#E1306C",
+    icon: <Instagram />,
+  },
+  {
+    name: "Facebook",
+    url: "https://www.facebook.com/TSLSilesia",
+    color: "#3b5998",
+    icon: <Facebook />,
+  },
+  {
+    name: "Telegram",
+    url: "http://t.me/WiktoriaKwiatkowska",
+    color: "#0088cc",
+    icon: <Telegram />,
+  },
+  {
+    name: "WhatsApp",
+    url: "https://wa.me/+48608675834",
+    color: "#25D366",
+    icon: <WhatsApp />,
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/company/104711986/admin/dashboard/",
+    color: "#0077B5",
+    icon: <LinkedIn />,
+  },
+  {
+    name: "YouTube",
+    url: "https://www.youtube.com/@tslsilesia9930",
+    color: "#FF0000",
+    icon: <YouTube />,
+  },
 ];
 
 const SocialMediaList: React.FC = () => {
   return (
-    <Box sx={{ display: "flex", justifyContent:"center", gap: 2 }}>
+    <Box
+      sx={{
+        display: "flex",
+        height: "80px",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 2,
+      }}
+    >
       {socialMediaList.map((social) => (
         <Box
           key={social.name}
