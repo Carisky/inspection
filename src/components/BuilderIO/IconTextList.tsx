d21136af734ecbcde0bb03ac747bd412febfc5ec
Yehor <egorkorsungit@gmail.com>
2025-03-03 12:42:01 +0100

    added appear animation for list elements (left to right)

--- src/components/BuilderIO/IconTextList.tsx
+++ src/components/BuilderIO/IconTextList.tsx
@@ -3,6 +3,7 @@ import { Box, Typography } from "@mui/material";
 import * as Icons from "@mui/icons-material";
 import { useLocaleStore } from "@/store/useLocaleStore";
 import pallete from "@/palette";
+import { motion } from "framer-motion";
 
 interface IconTextItem {
   iconName: string;
@@ -24,17 +25,27 @@ const IconTextList: React.FC<IconTextListProps> = ({ items = [] }) => {
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
       {items.map((item, index) => {
-        const IconComponent = (Icons as any)[item.iconName] || Icons.Info; // Если иконка не найдена, используем Info
+        const IconComponent = (Icons as any)[item.iconName] || Icons.Info;
         const text =
           item.translations[locale as keyof typeof item.translations] ||
           item.translations.ru ||
           "Текст не указан";
 
         return (
-          <Box key={index} sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-            <IconComponent sx={{ fontSize: 24, color: pallete.common_colors.main_color }} />
-            <Typography sx={{fontSize:"20px"}} variant="body1">{text}</Typography>
-          </Box>
+          <motion.div
+            key={index}
+            initial={{ x: "-100%", opacity: 0 }}
+            whileInView={{ x: "0%", opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, delay: index * 0.1 }}
+          >
+            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+              <IconComponent sx={{ fontSize: 24, color: pallete.common_colors.main_color }} />
+              <Typography sx={{ fontSize: "20px" }} variant="body1">
+                {text}
+              </Typography>
+            </Box>
+          </motion.div>
         );
       })}
     </Box>
