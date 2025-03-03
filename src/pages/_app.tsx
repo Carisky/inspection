7aa6f4061dde0415b1645383707b269661377258
Yehor <egorkorsungit@gmail.com>
2025-03-03 14:30:09 +0100

    elevate builder.init to app level for minimize fetching urls

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,16 @@ import type { AppProps } from "next/app";
 import Register from "@/Register";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
+import { builder } from "@builder.io/react";
+
+// Инициализация Builder.io в единственном экземпляре для всего приложения
+const apiKey = process.env.NEXT_PUBLIC_BUILDER_API_KEY;
+if (apiKey) {
+  builder.init(apiKey);
+} else {
+  console.error("Builder.io API key is not defined!");
+}
+
 Register.init();
 
 export default function App({ Component, pageProps }: AppProps) {
