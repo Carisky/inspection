7309289c2e196b6116706492d82a06376bab1857
Yehor <egorkorsungit@gmail.com>
2025-03-03 12:42:15 +0100

    added appear animation for title

--- src/components/BuilderIO/MultiLangTitle.tsx
+++ src/components/BuilderIO/MultiLangTitle.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useLocaleStore } from "@/store/useLocaleStore";
 import { Box, Divider, Typography } from "@mui/material";
 import pallete from "@/palette";
+import { motion } from "framer-motion";
 
 interface MultiLangTitleProps {
   translations?: {
@@ -26,7 +27,7 @@ const fontSizeMap: Record<"h1" | "h2" | "h3" | "h4" | "h5" | "h6", number> = {
 const MultiLangTitle: React.FC<MultiLangTitleProps> = ({
   translations = {},
   showDividers = true,
-  headingLevel = "h2", // По умолчанию h2
+  headingLevel = "h2",
 }) => {
   const { locale } = useLocaleStore();
 
@@ -43,23 +44,37 @@ const MultiLangTitle: React.FC<MultiLangTitleProps> = ({
     mergedTranslations[locale as keyof typeof mergedTranslations] ||
     mergedTranslations.ru;
 
+  // Варианты анимации: изначально заголовок смещён вверх и невидим, затем появляется
+  const variants = {
+    hidden: { opacity: 0, y: -20 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
-    <Box sx={{ width: "100%" }}>
-      {showDividers && <Divider sx={{ backgroundColor: "#01aaa0" }} />}
-      <Typography
-        variant={headingLevel} // Динамический заголовок
-        component={headingLevel}
-        sx={{
-          color: pallete.common_colors.main_color,
-          textAlign: "center",
-          fontSize: `${fontSizeMap[headingLevel]}px`, // Размер заголовка
-          fontWeight: 600,
-        }}
-      >
-        {text}
-      </Typography>
-      {showDividers && <Divider sx={{ backgroundColor: "#01aaa0" }} />}
-    </Box>
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      transition={{ duration: 0.75, ease: "easeOut" }}
+      variants={variants}
+    >
+      <Box sx={{ width: "100%" }}>
+        {showDividers && <Divider sx={{ backgroundColor: "#01aaa0" }} />}
+        <Typography
+          variant={headingLevel}
+          component={headingLevel}
+          sx={{
+            color: pallete.common_colors.main_color,
+            textAlign: "center",
+            fontSize: `${fontSizeMap[headingLevel]}px`,
+            fontWeight: 600,
+          }}
+        >
+          {text}
+        </Typography>
+        {showDividers && <Divider sx={{ backgroundColor: "#01aaa0" }} />}
+      </Box>
+    </motion.div>
   );
 };
 
