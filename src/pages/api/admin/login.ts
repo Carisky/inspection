258a8f035a3ee6d078849e73407b6557bc2706cc
Yehor <egorkorsungit@gmail.com>
2025-11-28 09:13:34 +0100

    deleted admin

--- src/pages/api/admin/login.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { adminController } from "../../../backend/controllers/adminController";
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  return adminController.login(req, res);
-}
